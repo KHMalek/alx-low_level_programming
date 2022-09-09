fd13d236f395d3494e2478dae3703164b10547cf
KHMalek <khemirimelek@gmail.com>
2022-09-09 12:08:38 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int X;
+
+	for (X = 0; X < 10; X++)
+	{
+	printf("%d", X);
+	}
+	printf("\n");
+	return (0);
+}
