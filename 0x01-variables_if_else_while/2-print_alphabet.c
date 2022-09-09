309aa78b2ef63922d0454b02083ca97d007d050a
KHMalek <khemirimelek@gmail.com>
2022-09-09 11:59:41 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char CH;
+
+	for (CH = 'a'; CH <= 'z'; CH++)
+	{
+	putchar (CH);
+	}
+	putchar ('\n');
+	return (0);
+}
