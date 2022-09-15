4b68217007f29e4dacd8ea825253ab65a74216aa
KHMalek <khemirimelek@gmail.com>
2022-09-15 12:41:38 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+int a, b;
+
+for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b <= 14; j++)
+		{
+		if (b >= 10)
+		_putchar(b / 10 + '0');
+
+		_putchar(b % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
