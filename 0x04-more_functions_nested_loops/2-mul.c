58091674f541b1c9f5ea77335a43e3aa6b38877d
KHMalek <khemirimelek@gmail.com>
2022-09-15 12:34:59 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: a is an integer
+ * @b: b is an integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+int x;
+
+x = (a * b);
+return (x);
+}
