89461b6edcec18a7c10bf23c5fdd5272d6e8f24b
KHMalek <khemirimelek@gmail.com>
2022-09-22 12:12:21 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rot13 - Change a string to  ROT format.
+ *
+ * @s: is a pointer to a char.
+ *
+ * Return: 0
+ */
+
+char *rot13(char *s)
+{
+	char input[1000] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[1000] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+return (s);
+}
