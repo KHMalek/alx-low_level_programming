dc8a3ff89be7380a079208c05209c38cc5522704
KHMalek <khemirimelek@gmail.com>
2022-09-26 10:37:24 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Write a function that locates a character in a string.
+ *
+ * @s: is a pointer to a char
+ * @c: is a pointer to a char
+ *
+ * Return: Returns the transformed pointer
+ *
+**/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+	s++;
+	}
+if (*s == c)
+return (s);
+
+return (0);
+}
