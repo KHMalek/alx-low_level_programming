96e39a57a4a5336ebf7017baf9609b031ddec01b
KHMalek <khemirimelek@gmail.com>
2022-10-10 11:38:42 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -57,4 +57,39 @@ return (dest);
 **/
 
 dog_t *new_dog(char *name, float age, char *owner)
+{
+char *newName;
+char *newOwner;
+dog_t *newDog;
 
+newDog = malloc(sizeof(struct dog));
+
+if (newDog == NULL)
+{
+	return (NULL);
+}
+
+newName = malloc((_strlen(name) + 1));
+if (newName == NULL)
+{
+	free(newDog);
+	return (NULL);
+}
+
+newOwner = malloc((_strlen(owner) + 1));
+if (newOwner == NULL)
+{
+	free(newName);
+	free(newDog);
+	return (NULL);
+}
+
+newName = _strncpy(newName, name);
+newOwner = _strncpy(newOwner, owner);
+
+newDog->name = newName;
+newDog->age = age;
+newDog->owner = newOwner;
+
+return (newDog);
+}
