f665cd376ef30d770e095e248f418f70b2837c33
KHMalek <khemirimelek@gmail.com>
2022-10-25 12:47:42 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,56 +1,34 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * pop_listint - Deletes the head node and returns the head node's data
- * @head: pointer to a struc
- * Return: Returns nothing
-**/
-int pop_listint(listint_t **head)
-{
-	int i = 0;
-	listint_t *tmp;
-
-	if (*head == NULL)
-		return (0);
-
-	tmp = *head;
-	i = tmp->n;
-	*head = (*head)->next;
-
-	free(tmp);
-	return (i);
-}
-
-/**
- * delete_nodeint_at_index - Deletes a node in a specific position
- * @head: Pointer to a pointer pointing to a struct
- * @index: Index of the list where the new node should be deleted
- * Return: 1 if success or -1 if not
-**/
+ * delete_nodeint_at_index - deletes a node in a linked list at a certain index
+ * @head: pointer to the first element in the list
+ * @index: index of the node to delete
+ * Return: 1 (Success), or -1 (Fail)
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp = *head, *nextNode;
-	unsigned int i;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
-		pop_listint(head);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
-
-	for (i = 0; tmp != NULL && i < (index - 1); i++)
-		tmp = tmp->next;
-
-	if (tmp == NULL || tmp->next == NULL)
-		return (-1);
-
-	nextNode = (tmp->next)->next;
-	free(tmp->next);
-	tmp->next = nextNode;
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
 	return (1);
 }
