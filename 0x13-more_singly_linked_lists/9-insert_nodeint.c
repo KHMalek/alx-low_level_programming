8d2e9af2c78680115bd318c22bab3dd6b28f4ea7
KHMalek <khemirimelek@gmail.com>
2022-10-25 12:41:07 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,59 +1,40 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint - Adds a new node
- * @head: Pointer to a pointer of a struct constant
- * @n: int constant
- * Return: The number of nodes
-**/
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-listint_t *newNode;
-
-	newNode = malloc(sizeof(newNode));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->n = n;
-	newNode->next = *(head);
-	*head = newNode;
-
-return (newNode);
-}
-
-/**
- * insert_nodeint_at_index - Inserts a node in a specific position
- * @head: Pointer to a pointer pointing to a struct
- * @idx: Index of the list where the new node should be added
- * @n: value of the int passed to the new node
- * Return: The address of the new node
-**/
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ * Return: pointer to the new node, or NULL
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	if (head == NULL)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
-
+	new->n = n;
+	new->next = NULL;
 	if (idx == 0)
-		return (add_nodeint(head, n));
-
-	while (*head != NULL)
 	{
-		if (idx == i)
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
 		{
-			newNode = malloc(sizeof(newNode));
-			if (newNode == NULL)
-				return (NULL);
-
-			newNode->n = n;
-			newNode->next = *head;
-			*head = newNode;
-			return (newNode);
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
-	head = &(*head)->next;
-	i++;
+		else
+			temp = temp->next;
 	}
 	return (NULL);
 }
